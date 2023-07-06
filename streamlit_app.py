3a607d0423d3d2767df600638d055ce9e189f829
Frederik Loy <130966694+Loy-Frederik@users.noreply.github.com>
2023-07-06 17:17:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-logo = Image.open('https://img.freepik.com/premium-vector/cute-couple-panda-watching-movie-eating-popcorn-cartoon-vector-icon-illustration-animal-food_138676-6443.jpg')
+# logo = Image.open('https://img.freepik.com/premium-vector/cute-couple-panda-watching-movie-eating-popcorn-cartoon-vector-icon-illustration-animal-food_138676-6443.jpg')
 
 st.set_page_config(page_title='WBSFlix', page_icon=logo)
 st.header("Find awesome movies")
