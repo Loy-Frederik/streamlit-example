8d57a85b65c88e3a216af2acdc511eaf552fd8dc
Frederik Loy <130966694+Loy-Frederik@users.noreply.github.com>
2023-07-06 21:18:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,13 @@ from io import BytesIO
 
 logo_url = 'https://img.freepik.com/premium-vector/cute-couple-panda-watching-movie-eating-popcorn-cartoon-vector-icon-illustration-animal-food_138676-6443.jpg'
 
+rating_url = 'https://drive.google.com/file/d/1JBolFNkww-nRO_PTAHeXGE3KSVY4_7dl/view?usp=sharing'
+titles_url = 'https://drive.google.com/file/d/1Z3vHbjAeTAmFp-NeM-j4zYJjLz-fpqfn/view?usp=sharing'
+
+path = 'https://drive.google.com/uc?export=download&id='
+rating_df = pd.read_csv(path+rating_url.split('/')[-2])
+titles_df = pd.read_csv(path+titles_url.split('/')[-2])
+
 # Download the image using requests
 response = requests.get(logo_url)
 image_bytes = response.content
@@ -21,4 +28,7 @@ st.set_page_config(page_title='WBSFlix', page_icon=logo)
 
 st.header("Find awesome movies")
 
-
+st.sidebar.header('What do you wanna do?')
+def movies_like():
+  movie = st.sidebar.selectbox('Movie like', titles_df['title'])
+   
